a991d20f0bbf2e7d55c7633357f07c98c9da077a
Jonathan Mores <jonathanmopro@gmail.com>
2022-03-14 13:16:55 -0400

    réglages de tout les problemes encore

--- footer.php
+++ footer.php
@@ -1,32 +1,30 @@
 <footer class="site__footer">
-    <section class="site__footer__aPropos">
-    <h2 class="footer__titre">Footer</h2>
-    <p class="footer__presentation">Un site web fait par un élève du TIM à l'aide de Wordpress.</p>
-    <h3 class="footer__author">Modifié par Jonathan Mores et les élèves du programme de TIM</h3>
-
-</section>
-
-<section class="site__footer__liens">
-    <h2 class="footer__titre">Sevices</h2>
-    <?php 
-    $icone = '<svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#f00"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>';
-    wp_nav_menu(array(
-                        "menu"=>"simple",
-                        "menu_class"=>"site__footer__liens__ul",
-                        "link_before"=>$icone));
-    ?>
-</section>
-
-<section class="site__footer__liens">
-    <h2 class="footer__titre">Médias</h2>
-    <?php 
-    $icone = '<svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#f00"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>';
-    wp_nav_menu(array(
-                        "menu"=>"simple",
-                        "menu_class"=>"site__footer__liens__ul",
-                        "link_before"=>$icone)); 
-    ?>
-</section>
+<div class="site__footer__colonne">
+    <section class="footer__adresse">
+        <?php get_sidebar( 'pied_page_colonne_2' ); ?>
+    </section>
+    <section class="footer__article">
+        <?php get_sidebar( 'pied_page_colonne_1' ); ?>
+    </section>
+    <section class="footer__lien"><?php wp_nav_menu(array("menu"=>"lien_externe")); ?></section>
+</div>
+<div class="site__footer__ligne">
+    <section class="footer__description">4w4-Conception d'interface et développement Web</section>
+    <section class="footer__copyright">&copy; Collège de Maisonneuve - Tous droit réservé</section>
+    <section class="footer__menu">
+        <?php
+        $icone = '<svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#f00"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>';
+        wp_nav_menu(array(
+                    "menu"=>"simple",
+                    "container"=>"nav",
+                    "container_class"=>"footer__menu__nav",
+                    "menu_class"=>"footer__menu__nav__ul",
+                    "link_before"=>$icone)); ?>
+    </section>
+    <section class="footer__recherche"><?php get_search_form(); ?></section>
+    <section class="footer__auteur">Auteur : Jonathan Mores</section>
+</div>
 </footer>
+<?php wp_footer() ?>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -1,11 +1,5 @@
 <?php 
 
-add_action('get_header', 'remove_admin_login_header');
-
-function remove_admin_login_header() {
-    remove_action('wp_head', '_admin_bar_bump_cb');
-}
-
 function cidw_4w4_enqueue(){
     //wp_enqueue_style('style_css', get_stylesheet_uri());
     wp_enqueue_style('4w4-le-style', get_template_directory_uri() . '/style.css', array(), filemtime(get_template_directory() . '/style.css'), false);
@@ -18,6 +12,7 @@ function cidw_4w4_register_nav_menu(){
     register_nav_menus( array(
         'menu_principal' => __( 'Menu principal', 'cidw_4w4' ),
         'menu_footer'  => __( 'Menu footer', 'cidw_4w4' ),
+        'menu_lien_externe'  => __( 'Menu lien externe', 'cidw_4w4' ),
     ) );
 }
 add_action( 'after_setup_theme', 'cidw_4w4_register_nav_menu', 0 );
@@ -34,6 +29,80 @@ function cidw_4w4_filtre_choix_menu($obj_menu){
     }
     return $obj_menu;
 }
-add_filter("wp_nav_menu_objects","cidw_4w4_filtre_choix_menu")
+add_filter("wp_nav_menu_objects","cidw_4w4_filtre_choix_menu");
+/* -----------------------------------------------------------   add_theme_support() */
+function cidw_4w4_add_theme_support()
+{
+    add_theme_support('post-thumbnails');
+
+    add_theme_support("custom-logo", array(
+        "width" => 100,
+        "height" => 100,
+    ));
+
+}
+
+
+
+
+/*---------------------------------------------------------- Enregistrement des sidebar */
+
+
+function my_register_sidebars() {
+    /* Register the 'primary' sidebar. */
+    register_sidebar(
+        array(
+            'id'            => 'pied_page_colonne_1',
+            'name'          => __( 'Pied de page colonne 1' ),
+            'description'   => __( 'Colonne de pied de page' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+        )
+    );
+
+    register_sidebar(
+        array(
+            'id'            => 'pied_page_colonne_2',
+            'name'          => __( 'Pied de page colonne 2' ),
+            'description'   => __( 'Colonne de pied de page' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+        )
+    );
+
+    register_sidebar(
+        array(
+            'id'            => 'pied_page_colonne_3',
+            'name'          => __( 'Pied de page colonne 3' ),
+            'description'   => __( 'Colonne de pied de page' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+        )
+    );
+
+    register_sidebar(
+        array(
+            'id'            => 'pied_page_ligne_1',
+            'name'          => __( 'Pied de page ligne 1' ),
+            'description'   => __( 'Colonne de pied de page' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+        )
+    );
+    /* Repeat register_sidebar() code for additional sidebars. */
+}
+
+add_action( 'widgets_init', 'my_register_sidebars' );
+
+?>
+
 
 ?>
