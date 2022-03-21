329cf284566a9dd56f5fd74f30521e164b656be6
Jonathan Mores <jonathanmopro@gmail.com>
2022-03-20 22:58:44 -0400

    s7-c2 JM modifications des single post

--- single-post.php
+++ single-post.php
@@ -1,13 +1,13 @@
 <?php get_header() ?>
 <main class="site__main">
-     <h1>-------  single-post.php ----------</h1>
+     
      <?php if (have_posts()) : while(have_posts()) : the_post(); ?>
           <article class="cours">
               
                <h1 class="cours__titre">
                     <?php the_title(); ?>
                </h1>   
-               <section>
+               <section class="cours__contenu">
                     <?php the_post_thumbnail("thumbnail"); ?> 
                     <?php  the_content(); ?>
                </section>
