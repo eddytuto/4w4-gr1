c69b27f7fbb1dd13e8dce0b262dbe11fa2dc2596
Eddy Martin <eddypmartin@gmail.com>
2022-04-28 21:55:46 -0400

    s13-c1 bouton burger animé avec clip-path transition CSS et sans javascript burger.js a été retiré

--- functions.php
+++ functions.php
@@ -11,16 +11,6 @@ function cidw_4w4_enqueue(){
                         array(),
                         filemtime(get_template_directory() . '/javascript/boite-modale.js'),
                         true);
-
-    wp_enqueue_script('cidw-4w4-burger', 
-                        get_template_directory_uri() . '/javascript/burger.js',
-                        array(),
-                        filemtime(get_template_directory() . '/javascript/burger.js'),
-                        true);                    
-   
-   
-    
-
 }
 
 add_action("wp_enqueue_scripts", "cidw_4w4_enqueue");
