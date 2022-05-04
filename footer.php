bf54664240a70bf16ead8cac65d03580d8d19dc6
Eddy Martin <eddypmartin@gmail.com>
2022-05-04 17:58:46 -0400

    s14-c1 Carrousel amélioré avec object.fit et bouton de navigation pour accéder à une nouvelle image

--- footer.php
+++ footer.php
@@ -26,6 +26,10 @@
     <button class="boite__modale__ferme">X</button>
     <article class="boite__modale__texte"></article>
 </div>
+<div class="boite__carrousel">
+    <button class="boite__carrousel__ferme">X</button>
+    <section class="boite__carrousel__navigation"></section>
+</div>
 <?php wp_footer(); ?>
 </body>
 </html>
--- front-page.php
+++ front-page.php
@@ -13,7 +13,6 @@
    <?php if (have_posts()): the_post(); ?>
         <h2><?php the_title() ?></h2>
         <?php the_content() ?>   
-  
    <?php endif ?>
    
 </main>
